5e77bc4531566ad782a266674aad65abdaa7c28f
Kumar Kasimala <kkasimala@salesforce.com>
2018-09-22 16:50:57 -0700

    New Changes

--- data.js
+++ data.js
@@ -1,6 +1,12 @@
 ﻿
 window.devs = [
 {name:"Kumar", age:"30", contact:"28383833", Id:"12", emailId:"kumarK@gmail.com"},
+{name:"Sandeep", age:"20", contact:"dd", Id:"13", emailId:"sandeep@gmail.com"},
+{name:"Kumar", age:"30", contact:"28383833", Id:"12", emailId:"kumarK@gmail.com"},
+{name:"Sandeep", age:"20", contact:"dd", Id:"13", emailId:"sandeep@gmail.com"},
+{name:"Kumar", age:"30", contact:"28383833", Id:"12", emailId:"kumarK@gmail.com"},
+{name:"Sandeep", age:"20", contact:"dd", Id:"13", emailId:"sandeep@gmail.com"},
+{name:"Kumar", age:"30", contact:"28383833", Id:"12", emailId:"kumarK@gmail.com"},
 {name:"Sandeep", age:"20", contact:"dd", Id:"13", emailId:"sandeep@gmail.com"}
 ];
 
