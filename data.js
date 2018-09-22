ea79865f46bdfcb0b18a935ff18bdc05b68681de
SurajGaur <gaursuraj1029@gmail.com>
2018-09-22 16:38:37 -0700

    QA's and Managers added

--- data.js
+++ data.js
@@ -1,16 +1,17 @@
 ﻿
 window.devs = [
-{name:"Kumar", age:"30", contact:"28383833", Id:"12", emailId:"kumarK@gmail.com"},
-{name:"Sandeep", age:"20", contact:"dd", Id:"13", emailId:"sandeep@gmail.com"}
-];
-
-window.qas = [
-{name:"Kumar", age:"30", contact:"28383833", Id:"12", emailId:"kumarK@gmail.com"},
-{name:"Sandeep", age:"20", contact:"dd", Id:"13", emailId:"sandeep@gmail.com"}
-]
-
-window.managers = [
-{name:"Kumar", age:"30", contact:"28383833", Id:"12", emailId:"kumarK@gmail.com"},
-{name:"Sandeep", age:"20", contact:"dd", Id:"13", emailId:"sandeep@gmail.com"}
-]
-
+    {name:"Kumar", age:"30", contact:"22222222", Id:"12", emailId:"kumarKas@gmail.com"},
+    {name:"Sandeep", age:"20", contact:"dd", Id:"13", emailId:"sandeep@gmail.com"}
+    ];
+    
+    window.qas = [
+    {name:"QaKumar", age:"30", contact:"28383833", Id:"12", emailId:"kumarK@gmail.com"},
+    {name:"QaSandeep", age:"20", contact:"122222222", Id:"13", emailId:"sandeep@gmail.com"}
+    ]
+    
+    window.mngrs = [
+    {name:"MnKumar", age:"30", contact:"28383833", Id:"12", emailId:"kumarK@gmail.com"},
+    {name:"MnSandeep", age:"20", contact:"dd", Id:"13", emailId:"sandeep@gmail.com"}
+    ]
+    
+    
